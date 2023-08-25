2684e8d2dfaa07357ce15106769b90050b70ad76
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-08-25 23:54:36 +0000

    Added install-python3, fix flask version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,18 @@
 #install flask from pip3.
 
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update'
+exec { 'install python3':
+    commnad => '/usr/bin/apt-get install -y python3',
+    onlyif  => '/usr/bin/apt-get update'
 }
 
 package { 'python3-pip':
     ensure  => 'installed',
-    require => Exec['apt-get update']
+    require => Exec['install python3']
 }
 
 exec { 'install-flask==2.1.0':
-    command => '/usr/bin/pip3 install Flask=2.1.0',
-    onlyif  => '/usr/bin/pip3'
+    command => '/usr/bin/pip3 install Flask==2.1.0',
+    onlyif  => '/usr/bin/which pip3 '
 }
 
 Exec['install-flask==2.1.0'] -> Package['python3-pip']
