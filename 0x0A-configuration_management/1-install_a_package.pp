01fb151d90cda891375ff9865ba523b39f46f8d2
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-08-26 00:07:01 +0000

    Fix typo error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #install flask from pip3.
 
 exec { 'install python3':
-    commnad => '/usr/bin/apt-get install -y python3',
+    command => '/usr/bin/apt-get install -y python3',
     onlyif  => '/usr/bin/apt-get update'
 }
 
@@ -12,7 +12,7 @@ package { 'python3-pip':
 
 exec { 'install-flask==2.1.0':
     command => '/usr/bin/pip3 install Flask==2.1.0',
-    onlyif  => '/usr/bin/which pip3 '
+    onlyif  => '/usr/bin/which pip3'
 }
 
 Exec['install-flask==2.1.0'] -> Package['python3-pip']
