41c0be82045e629590a9ed5baa10ac9e90617af0
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-08-28 02:40:16 +0000

    Fix another bug

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,11 @@
 #  set up your client SSH configuration file so that you can connect to a server without typing a password using the private key ~/.ssh/school
 
-file_line { 'Configure identity file':
-  path    => '~/.ssh/config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^#*\s*IdentityFile',
-  ensure  => present,
+exec {'Turn off passwd auth':
+  path    => '/usr/bin:/bin',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config'
 }
 
-file_line { 'Disable password authentication':
-  path    => '~/.ssh/config',
-  line    => 'PasswordAuthentication no',
-  match   => '^#*\s*PasswordAuthentication',
-  ensure  => present,
+exec {'Declare identity file':
+  path    => '/usr/bin:/bin',
+  command => 'echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
 }
