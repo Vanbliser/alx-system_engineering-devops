e35a11a702198edb3b168d9220d19e4dd5f95f37
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-08-28 02:01:11 +0000

    Add task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,15 @@
 #  set up your client SSH configuration file so that you can connect to a server without typing a password using the private key ~/.ssh/school
 
-file {
+include stdlib
+
+file_line { 'Declare identity file':
+ path 	=> '/etc/ssh/ssh_config',
+ line	=> ' IdentifyFile ~/.ssh/school',
+ replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+ path	=> '/etc/ssh/ssh_config',
+ line	=> ' PasswordAuthentication no',
+ replace => true,
+} 
