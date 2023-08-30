2d491f3545fdaa9e7e83595d7fdb76802e7b2cc0
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-08-30 11:36:42 +0000

    Update task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,16 +1,18 @@
-# Puppet manifest to install nginx and configure the server
+# Puppet manifest to install nginx and configure the server with custom home page and redirect on path /redirect_me
+
 package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'Redirect':
+file_line { 'redirect_me':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me/?$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-file { '/var/www/html/index.html':
+file { 'home_page':
+  path    => '/var/www/html/index.html',
   content => 'Hello World!',
 }
 
@@ -18,3 +20,5 @@ service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
 }
+
+Package['nginx'] -> File_line['redirect_me'] -> File['home_page'] -> Service['nginx']
